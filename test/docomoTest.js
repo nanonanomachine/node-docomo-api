635d306ada0a63c3c60ccbcf5364038ecac72c53
nanonanomachine <koyama.gakugai@gmail.com>
2014-12-22 20:48:59 +0900

    Initial import

---
+++ test/docomoTest.js
@@ -0,0 +1,65 @@
+var should = require('should');
+var fs = require('fs');
+var path = require('path');
+var config = JSON.parse(fs.readFileSync(path.normalize(__dirname + '/config.json', 'utf8')));
+
+describe('Docomo API', function(){
+	var Docomo = require('../lib/docomo');
+
+	describe('createDialogue', function(){
+		it('should get utt data without optional params', function(done){
+			var api = new Docomo(config.apiKey);
+			api.createDialogue('Hello', function(error, data){
+				data.utt.should.not.equal('');
+				done();
+			});
+		});
+
+		it('should get utt data with optional params', function(done){
+			var api = new Docomo(config.apiKey);
+			var params = {
+				 "nickname": "光",
+				 "nickname_y": "ヒカリ",
+				 "sex": "女",
+				 "bloodtype": "B",
+				 "birthdateY": "1997",
+				 "birthdateM": "5",
+				 "birthdateD": "30",
+				 "age": "16",
+				 "constellations": "双子座",
+				 "place": "東京"
+			};
+
+			api.createDialogue('Hello', params, function(error, data){
+				data.utt.should.not.equal('');
+				done();
+			});
+		});
+
+		it('should set context after getting response', function(done){
+			var api = new Docomo(config.apiKey);
+			api.createDialogue('Hello', function(error, data){
+				should(api.context).be.ok;
+				done();
+			});
+		});
+
+		it('should set context undefined when failing request', function(done){
+			var api = new Docomo(config.apiKey);
+			api.apiKey = null;
+			api.createDialogue(null,function(error,data){
+				should(api.context).be.not.ok;
+				done();
+			})
+		});
+
+		it('should get error when apiKey is not set', function(done){
+			var api = new Docomo(config.apiKey);
+			api.apiKey = null;
+			api.createDialogue(null, function(error, data){
+				should(error).be.ok;
+				done();
+			});
+		});
+	});
+});
